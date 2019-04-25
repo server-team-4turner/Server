2d9af98ef6340b434006d2ddab772a70fabe63c7
William Gardner <wg4568@gmail.com>
2019-04-25 12:05:17 -0700

    reverse string

--- testServer.py
+++ testServer.py
@@ -1,11 +1,9 @@
 from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	name = request.args.get('name')
-	message = 'Hello, ' + name + '!'
-	return message
+@app.route('/reverse/<word>')
+def index(word):
+	return word[::-1]
 
 # browse to localhost:8000/?name=William
 app.run('0.0.0.0', port=8000, debug=True)
