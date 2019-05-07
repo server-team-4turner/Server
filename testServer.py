609b5a30d8c5733d1cbe211b223b33fddd2c5158
William Gardner <wg4568@gmail.com>
2019-05-07 11:16:05 -0700

    added random method

--- testServer.py
+++ testServer.py
@@ -1,9 +1,17 @@
 from flask import Flask, request
+import random
 app = Flask(__name__)
 
 @app.route('/reverse/<word>')
-def index(word):
+def _reverse(word):
 	return word[::-1]
+
+@app.route('/random/<int:quantity>')
+def _random(quantity):
+	response = []
+	for i in range(quantity):
+		response.append(random.randint(0, 100))
+	return ' '.join(map(str, response))
 
 # browse to localhost:8000/?name=William
 app.run('0.0.0.0', port=8000, debug=True)
